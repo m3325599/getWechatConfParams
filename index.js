d568f7324aedc7396351f8bcadad0273082235c2
王垠舒 <wangys@jiedaibao.com>
2019-02-22 11:52:43 +0800

    添加测试

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
 (function () {
-	const map = {
-		'feplanet.jdburl.com': 'wxb94031e4971ddfa7',
-		'plg.jdburl.com': 'wxb94031e4971ddfa7',
-		'share.jdburl.com': 'wxb94031e4971ddfa7',
-		'feplanet.jiedaibao.com.cn': 'wx142281877b9889f4',
-		'plg.jiedaibao.com.cn': 'wx142281877b9889f4',
-		'share.jiedaibao.com.cn': 'wx142281877b9889f4',
-		'www.jiedaibao.com': 'wxc9a3c8c03a7fd87c',
-		'investapi.jiedaibao.com.cn': 'wxc9a3c8c03a7fd87c',
-	}
+  const map = {
+    'feplanet.jdburl.com': 'wxb94031e4971ddfa7',
+    'plg.jdburl.com': 'wxb94031e4971ddfa7',
+    'share.jdburl.com': 'wxb94031e4971ddfa7',
+    'feplanet.jiedaibao.com.cn': 'wx142281877b9889f4',
+    'plg.jiedaibao.com.cn': 'wx142281877b9889f4',
+    'share.jiedaibao.com.cn': 'wx142281877b9889f4',
+    'www.jiedaibao.com': 'wxc9a3c8c03a7fd87c',
+    'investapi.jiedaibao.com.cn': 'wxc9a3c8c03a7fd87c',
+  }
 
-	function ajax (url, method) {
-		if (window) {
-			const getLocalHref = location.host
-			const protocol = location.protocol
-			const pageUrl = location.href
-			const shareUrl = `http://tongji.jiedaibao.com/weixin/config?current_page_url=${pageUrl}&app_id=${map[getLocalHref]}`
-		}
-		try {
-			if (!map[getLocalHref]) {
-				throw '非白名单,不支持分享此域名'
-			}
-		} catch (e) {
-			console.error(e)
-		}
+  function getHost (uri) {
+    if (!uri) {
+      throw '没有分享链接，无法获取host'
+    }
+    const arr = uri.split('//')
+    return arr[arr.length - 1].split('/')[0]
+  }
 
-		let promiseObj = new Promise(function (resolve, rejext) {
-			const http = new XMLHttpRequest();
-			http.open(method || 'POST', url, true);
-			http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-			http.send();
+  function ajax (url, method) {
+    const protocol = location.protocol
+    const pageUrl = url || location.href
+    const shareUrl = `http://tongji.jiedaibao.com/weixin/config?current_page_url=${pageUrl}&app_id=${map[getHost(pageUrl)]}`
+    
+    try {
+      if (!map[getHost(pageUrl)]) {
+        throw '非白名单,不支持分享此域名'
+      }
+    } catch (e) {
+      console.error(e)
+    }
 
-			http.onreadystatechange = function() {
-			    if( http.readyState == 4 ) {
-			    	if ( http.status == 200 ) {
-			    		resolve(JSON.parse(http.responseText));
-			    	} else {
-			    		reject(xhr.status)
-			    	}
-			    }
-			}
-		})
-		return promiseObj;
-	}
+    let promiseObj = new Promise(function (resolve, reject) {
+      const http = new XMLHttpRequest();
+      http.open(method || 'POST', url, true);
+      http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      http.send();
 
-	module.exports = function (configUrl) {
-		if (configUrl) {
-			return ajax(configUrl)
-		} else {
-			return ajax(shareUrl)
-		}
-	}
+      http.onreadystatechange = function() {
+          if( http.readyState == 4 ) {
+            if ( http.status == 200 ) {
+              resolve(JSON.parse(http.responseText));
+            } else {
+              reject(http.status)
+            }
+          }
+      }
+    })
+    return promiseObj;
+  }
+
+  module.exports = function (conf) {
+    if (conf) {
+      return ajax(conf.url, conf.method)
+    } else {
+      return ajax()
+    }
+  }
 
 })()
 
--- index.test.js
+++ index.test.js
@@ -1,13 +1,21 @@
-const path = require('path')
-const expect = require('chai').expect;
-const getWechatConfig = require(./index.js)
+const nock = require('nock');
+const getWechatConfig = require('./index.js')
 
 const testUrl = 'http://tongji.jiedaibao.com/weixin/config?current_page_url=http%3A%2F%2Finvestapi.jiedaibao.com.cn%2Finvest-article%2Fshare&app_id=wxc9a3c8c03a7fd87c'
 
-describe('是否可以获取分享参数', function() {
-  it('分享参数对应字段应该有值', function(done) {
-  	const rst = await getWechatConfig(testUrl)
-  	expect(rst).should.not.be.empty
-  	// expect(rst).to.have.property('')
-  });
-});
+describe('getWechatConfig 的jest 测试', () => {
+	it('分享参数对应字段应该有值', () => {
+		nock('http://tongji.jiedaibao.com')
+			.defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/weixin/config?current_page_url=http%3A%2F%2Finvestapi.jiedaibao.com.cn%2Finvest-article%2Fshare&app_id=wxc9a3c8c03a7fd87c')
+      .reply(200, {"appId":"wxc9a3c8c03a7fd87c","nonceStr":"kMpOJSHom80lR1Gu","timestamp":1550805435,"signature":"6513f6e50a15212a02ed7cc4d9a934dbf27c127f"});
+		
+		return getWechatConfig({
+			url: testUrl,
+			method: 'GET'
+		}).then(data => {
+			expect(data).toBeDefined()
+	    expect(data.appId).toEqual('wxc9a3c8c03a7fd87c')
+		})
+	})
+})
