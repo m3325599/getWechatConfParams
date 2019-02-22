a7e55fd6125268ef1d54a113028974b7bf0e7a0a
王垠舒 <wangys@jiedaibao.com>
2019-02-22 15:29:28 +0800

    添加注释

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function () {
+  'use strict'
   const map = {
     'feplanet.jdburl.com': 'wxb94031e4971ddfa7',
     'plg.jdburl.com': 'wxb94031e4971ddfa7',
@@ -9,7 +10,11 @@
     'www.jiedaibao.com': 'wxc9a3c8c03a7fd87c',
     'investapi.jiedaibao.com.cn': 'wxc9a3c8c03a7fd87c',
   }
-
+  /**
+   * 根据URI获取Host
+   * @param  {uri} uri 完整的URI
+   * @return {String}     返回Host
+   */
   function getHost (uri) {
     if (!uri) {
       throw '没有分享链接，无法获取host'
@@ -17,11 +22,16 @@
     const arr = uri.split('//')
     return arr[arr.length - 1].split('/')[0]
   }
-
+  /**
+   * 根据URL获取微信配置参数
+   * @param  {String} url    分享URL
+   * @param  {String} method http: or https:
+   * @return {Object}        Promise
+   */
   function ajax (url, method) {
-    const protocol = location.protocol
-    const pageUrl = url || location.href
-    const shareUrl = `http://tongji.jiedaibao.com/weixin/config?current_page_url=${pageUrl}&app_id=${map[getHost(pageUrl)]}`
+    const protocol = location.protocol || 'http:'
+    const pageUrl = decodeURIComponent(url || location.href)
+    const shareUrl = `${protocol}//tongji.jiedaibao.com/weixin/config?current_page_url=${encodeURIComponent(pageUrl)}&app_id=${map[getHost(pageUrl)]}`
     
     try {
       if (!map[getHost(pageUrl)]) {
@@ -33,7 +43,7 @@
 
     let promiseObj = new Promise(function (resolve, reject) {
       const http = new XMLHttpRequest();
-      http.open(method || 'POST', url, true);
+      http.open(method || 'POST', shareUrl, true);
       http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       http.send();
 
@@ -49,7 +59,7 @@
     })
     return promiseObj;
   }
-
+  
   module.exports = function (conf) {
     if (conf) {
       return ajax(conf.url, conf.method)
--- index.test.js
+++ index.test.js
@@ -1,15 +1,23 @@
 const nock = require('nock');
 const getWechatConfig = require('./index.js')
 
-const testUrl = 'http://tongji.jiedaibao.com/weixin/config?current_page_url=http%3A%2F%2Finvestapi.jiedaibao.com.cn%2Finvest-article%2Fshare&app_id=wxc9a3c8c03a7fd87c'
+const testUrl = 'http%3A%2F%2Finvestapi.jiedaibao.com.cn%2Finvest-article%2Fshare'
+
+// 接口返回数据示例
+const rst = {
+  "appId": "wxc9a3c8c03a7fd87c",
+  "nonceStr": "kMpOJSHom80lR1Gu",
+  "timestamp": 1550805435,
+  "signature": "6513f6e50a15212a02ed7cc4d9a934dbf27c127f"
+}
 
 describe('getWechatConfig 的jest 测试', () => {
 	it('分享参数对应字段应该有值', () => {
 		nock('http://tongji.jiedaibao.com')
 			.defaultReplyHeaders({ 'access-control-allow-origin': '*' })
       .get('/weixin/config?current_page_url=http%3A%2F%2Finvestapi.jiedaibao.com.cn%2Finvest-article%2Fshare&app_id=wxc9a3c8c03a7fd87c')
-      .reply(200, {"appId":"wxc9a3c8c03a7fd87c","nonceStr":"kMpOJSHom80lR1Gu","timestamp":1550805435,"signature":"6513f6e50a15212a02ed7cc4d9a934dbf27c127f"});
-		
+      .reply(200, rst);
+
 		return getWechatConfig({
 			url: testUrl,
 			method: 'GET'
